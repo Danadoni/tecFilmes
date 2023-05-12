89d8fcddafbdc1f237ceb997fbdb52f76f957ba0
unknown <danadoni.lima@gmail.com>
2023-05-12 09:18:29 -0300

    Flatlist

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
 import BannerFilmes from './src/components/bannerFilmes';
-
+import Filmes from './src/data/movies'
 import Header from './src/components/header';
 import SearchBar from './src/components/searchbar';
+
 
 export default function App() {
   return (
@@ -11,6 +12,25 @@ export default function App() {
      <Header></Header>
      <SearchBar></SearchBar>
      <BannerFilmes></BannerFilmes>
+
+     <View style = {{width:'90%'}}>
+    <FlatList
+    horizontal = {true}
+    data={Filmes}
+    keyExtractor={(item)=> item.id}
+    renderItem={({item})=> (
+
+      <Text> {item.nome}</Text>
+
+    )}
+    
+    
+    
+    
+    />
+
+
+     </View>
      
     </View>
   );
